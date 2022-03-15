dc46d252df012b8f85e02fe9c361bca794bf9fc1
moaslak <moaslak@gmail.com>
2022-03-15 08:45:30 +0100

    beløb NOT belob

--- Rap_Finands/Program.cs
+++ Rap_Finands/Program.cs
@@ -15,7 +15,7 @@ namespace Rap_Finands
         public static string reginummer = "4242";
         public static string datafil = "bank.json"; //Her ligger alt data i
         public static List<Konto> konti;
-        public static float belob;
+        //public static float belob; // denne er overflødig
         static void Main(string[] args)
         {
             Console.WriteLine("Henter alt kontodata");
@@ -152,7 +152,8 @@ namespace Rap_Finands
             if (saldo + beløb < 0) return false;
             var t = new Transaktion();
             t.tekst = tekst;
-            t.amount = belob;
+            //t.amount = belob; // der skal bruges argumentet
+            t.amount = beløb;
             t.saldo = t.amount + saldo;
             t.dato = DateTime.Now;
             
